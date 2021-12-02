24045c1988e26f0af076a9d37aeb02a487736966
SimonDanisch <sdanisch@gmail.com>
2021-12-02 16:20:43 +0100

    upgrade to 2.2.9

--- build/generate-master.jl
+++ build/generate-master.jl
@@ -2,13 +2,13 @@ using Clang
 using Clang.Generators
 
 cd(@__DIR__)
-
+# current version checked in is v2.2.9
 include_dir = normpath(joinpath(@__DIR__, "RadeonProRenderSDK", "inc"))
 # LIBCLANG_HEADERS are those headers to be wrapped.
 headers = joinpath.(include_dir, [
     "RadeonProRender_v2.h",
-    "RadeonProRender_GL.h",
-    "RadeonProRender_VK.h"
+    # "RadeonProRender_GL.h",
+    # "RadeonProRender_VK.h"
 ])
 
 # wrapper generator options
@@ -56,5 +56,13 @@ function rewrite!(dag::ExprDAG)
 end
 
 rewrite!(ctx.dag)
+
+quote
+    function check_error(error_code)
+        error_code == RPR_SUCCESS && return
+        return error("Error code returned: $(error_code)")
+    end
+end
+
 cd(@__DIR__)
 build!(ctx, BUILDSTAGE_PRINTING_ONLY)
