b9a31494d895af62c5741a78c170ead730dd1a74
SimonDanisch <sdanisch@protonmail.com>
2024-10-10 21:31:39 +0200

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,16 @@
 using RadeonProRender
 using Documenter
 
-DocMeta.setdocmeta!(RadeonProRender, :DocTestSetup, :(using RadeonProRender); recursive=true)
-
-makedocs(; modules=[RadeonProRender], authors="Simon Danisch",
-         repo="https://github.com/SimonDanisch/RadeonProRender.jl/blob/{commit}{path}#{line}",
-         sitename="RadeonProRender.jl",
-         format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
-                                canonical="https://SimonDanisch.github.io/RadeonProRender.jl", assets=String[]),
-         pages=["Home" => "index.md"])
+makedocs(;
+    modules=[RadeonProRender],
+    authors="Simon Danisch",
+    sitename="RadeonProRender.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://SimonDanisch.github.io/RadeonProRender.jl",
+        assets=String[]
+    ),
+    pages=["Home" => "index.md"]
+)
 
 deploydocs(; repo="github.com/SimonDanisch/RadeonProRender.jl")
